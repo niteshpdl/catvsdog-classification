4944e5ef36804ee578c628eb05c2ff260ab97527
Nitesh Paudel <niteshpaudel2004@gmail.com>
2025-07-03 10:36:06 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify, flash
 import os
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+from keras.models import load_model
+from keras.preprocessing import image
 import numpy as np
 import uuid
 import atexit
